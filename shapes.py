6509c78796c08945f93b803bfe252e19a54831d4
Bladee <Leonala@ssfs.org>
2023-09-21 13:48:20 -0400

    edited code

--- shapes.py
+++ shapes.py
@@ -1,20 +1,24 @@
 import turtle
 
-def draw_square(side_length, color):
-    turtle.color(color)
+def draw_an_octagon(color,size):
     turtle.begin_fill()
-
-    for x in range(4):
-        turtle.forward(side_length)
-        turtle.right(90)
+    for x in range(8):
+        turtle.color(color)
+        turtle.forward(size)
+        turtle.right(45)
     turtle.end_fill()
 
-draw_square(200, "green")
-turtle.penup()
-turtle.forward(300)
-turtle.pendown()
-draw_square(50, "gold")
+def move(x_coordinate, y_coordinate):
+    turtle.penup()
+    turtle.goto(x_coordinate, y_coordinate)
+    turtle.pendown()
 
-
+draw_an_octagon(color="blue", size=90)
+move(x_coordinate=0, y_coordinate=260)
+draw_an_octagon(color="pink", size=90)
+move(x_coordinate=-260, y_coordinate=260)
+draw_an_octagon(color="red", size=90)
+move(x_coordinate=-260, y_coordinate=-0)
+draw_an_octagon(color="purple", size=90)
 
 turtle.exitonclick()
